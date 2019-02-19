bc8d785141c3b16556cdb8a8deee044b443d2ae3
MuhtarMahmut <Muhtar.Mahmut0519@yahoo.com>
2019-02-19 12:41:35 -0500

    Day01
    Simple Testing

---
+++ src/test/java/Dpendency_Import/Dependencies.java
@@ -0,0 +1,46 @@
+package Dpendency_Import;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Dependencies {
+
+    public static void main(String[] args) {
+
+        // set up selenium environment
+        WebDriverManager.chromedriver().setup();
+
+        // declare webdriver veariable:
+     //   WebDriver driver=new WebDriver();        // WebDriver is an interface.
+        WebDriver driver=new ChromeDriver();
+           //    driver.get("http://www.facebook.com");          //in side get() must provide String value.
+                        // get: waits for the web fully uploaded.
+
+        driver.navigate().to("http://www.facebook.com");
+                    // Nevigate Does not wait for the web fully uploaded.
+            // Navigate is faster than get.
+      Mylibrary.sleep(3);    // Thread.sleep will make the java vertual machine to sleep
+
+        // in order to refresh:
+            driver.navigate().refresh();
+            new Actions(driver).pause(3000).perform(); // Action.pasue will make time outs for Selenium webdriver.
+                // in every actions we must put the .perform() at the end.
+
+            // goback to previous side:
+            driver.navigate().back();
+        Mylibrary.sleep(3);
+
+            // go forward:
+        driver.navigate().forward();
+        Mylibrary.sleep(3);
+
+     //   driver.close();   // closes the current tab
+
+        driver.quit();  // quites the webdriver.
+
+    }
+
+}
---
+++ src/test/java/Dpendency_Import/Mylibrary.java
@@ -0,0 +1,12 @@
+package Dpendency_Import;
+
+public class Mylibrary {
+
+    public static void sleep(double a){
+        try {
+            Thread.sleep((int)(a*1000));
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
